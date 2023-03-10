01b0a5914001fca82c7f061e7a443d523acb8301
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-03-10 20:46:47 -0300

    Initial Commit

---
+++ Poke/PokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApp.swift
+//  Poke
+//
+//  Created by Ailin Nakaganeku on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
